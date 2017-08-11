a7be9781269fa7d4e9f6e6ed824424678540f6af
nixzhu <zhuhongxu@gmail.com>
2017-08-11 15:16:59 +0800

    better attachments layout

--- Semigroup/ViewController.swift
+++ Semigroup/ViewController.swift
@@ -37,25 +37,44 @@ struct Feed: Codable {
     }
 }
 
+extension Array {
+    subscript(safe index: Int) -> Element? {
+        return indices ~= index ? self[index] : nil
+    }
+}
+
 class FeedCellNode: ASCellNode {
     let avatarImageNode = ASNetworkImageNode()
     let nicknameTextNode = ASTextNode()
     let createdAtTextNode = ASTextNode()
     let bodyTextNode = ASTextNode()
+    let attachmentImageNode1 = ASNetworkImageNode()
+    let attachmentImageNode2 = ASNetworkImageNode()
+    let attachmentImageNode3 = ASNetworkImageNode()
+
+    let feed: Feed
 
     init(feed: Feed) {
+        self.feed = feed
         super.init()
+
         backgroundColor = .white
 
         avatarImageNode.url = feed.creator.avatarURL
         nicknameTextNode.attributedText = NSAttributedString(string: feed.creator.nickname)
         createdAtTextNode.attributedText = NSAttributedString(string: "\(feed.createdAt)")
         bodyTextNode.attributedText = NSAttributedString(string: feed.body)
+        attachmentImageNode1.url = feed.attachments[safe: 0]?.imageURL
+        attachmentImageNode2.url = feed.attachments[safe: 1]?.imageURL
+        attachmentImageNode3.url = feed.attachments[safe: 2]?.imageURL
 
         addSubnode(avatarImageNode)
         addSubnode(nicknameTextNode)
         addSubnode(createdAtTextNode)
         addSubnode(bodyTextNode)
+        addSubnode(attachmentImageNode1)
+        addSubnode(attachmentImageNode2)
+        addSubnode(attachmentImageNode3)
     }
 
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
@@ -81,9 +100,38 @@ class FeedCellNode: ASCellNode {
             alignItems: .start,
             children: [stack2, bodyTextNode]
         )
+        let stack5 = ASStackLayoutSpec(
+            direction: .vertical,
+            spacing: 10,
+            justifyContent: .start,
+            alignItems: .start,
+            children: [stack3]
+        )
+        if !feed.attachments.isEmpty {
+            let stack4 = ASStackLayoutSpec(
+                direction: .horizontal,
+                spacing: 10,
+                justifyContent: .start,
+                alignItems: .start,
+                children: []
+            )
+            if feed.attachments[safe: 0] != nil {
+                attachmentImageNode1.style.preferredSize = CGSize(width: 100, height: 100)
+                stack4.children?.append(attachmentImageNode1)
+            }
+            if feed.attachments[safe: 1] != nil {
+                attachmentImageNode2.style.preferredSize = CGSize(width: 100, height: 100)
+                stack4.children?.append(attachmentImageNode2)
+            }
+            if feed.attachments[safe: 2] != nil {
+                attachmentImageNode3.style.preferredSize = CGSize(width: 100, height: 100)
+                stack4.children?.append(attachmentImageNode3)
+            }
+            stack5.children?.append(stack4)
+        }
         let inset = ASInsetLayoutSpec(
             insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10),
-            child: stack3
+            child: stack5
         )
         return inset
     }
@@ -117,21 +165,53 @@ class ViewController: ASViewController<ASTableNode> {
     }
 
     @objc private func addItem() {
-        let json = """
+        let json1 = """
             {
               "creator": {
                 "nickname": "NIX",
                 "avatar_url": "https://cdn-images-1.medium.com/fit/c/64/64/1*4ELWYhLQbcZXs1NkMMmd2Q@2x.jpeg"
               },
-              "body": "some string",
+              "body": "明月几时有，把酒问青天。不知天上宫阙，今夕是何年？我欲乘风归去，又恐琼楼玉宇，高处不胜寒。",
+              "attachments": [
+              ],
+              "created_at": 1500000000
+            }
+            """
+        let json2 = """
+            {
+              "creator": {
+                "nickname": "轻墨",
+                "avatar_url": "http://qingmo.me/images/readingbook.jpg"
+              },
+              "body": "生命好似被上帝精心编排的话剧，年少时学习，长大了工作，然后结婚生子，抚养小孩，照看父母，最后垂垂老去。一幕接着一幕，一幕赶着一幕。貌似自由意志，其实我们只有润色生活这么一点点权限。",
               "attachments": [
                 {
-                  "image_url": "https://cdn-images-1.medium.com/max/1600/1*LUPZaYjLsiziaMnRJwN4SA.png"
+                  "image_url": "http://qingmo.me/images/lifeisgood.jpg"
                 }
               ],
               "created_at": 1500000000
             }
             """
+        let json3 = """
+            {
+              "creator": {
+                "nickname": "NIX",
+                "avatar_url": "https://cdn-images-1.medium.com/fit/c/64/64/1*4ELWYhLQbcZXs1NkMMmd2Q@2x.jpeg"
+              },
+              "body": "其实我并不是一个爱冒险的人，而且结构工程师、设计院，也是不少人梦寐以求的，在社会上颇受尊重，但自己身在其中，总有太多的烦躁相伴，痛苦相随。痛苦是很有价值的感受，因为无论你怎么偏离你命中注定的航线，它都会一点一点地将你校正到正确的方向，即使你不勇敢，它也会让你变得义无反顾。",
+              "attachments": [
+                {
+                  "image_url": "http://shellhue.github.io/images/1.jpeg"
+                },
+                {
+                  "image_url": "http://shellhue.github.io/images/2.jpeg"
+                }
+              ],
+              "created_at": 1500000000
+            }
+            """
+        let jsons = [json1, json2, json3]
+        let json = jsons[Int(arc4random()) % jsons.count]
         let data = json.data(using: .utf8)!
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .secondsSince1970
@@ -150,7 +230,6 @@ extension ViewController: ASTableDataSource, ASTableDelegate {
 
     func tableNode(_ tableNode: ASTableNode, nodeForRowAt indexPath: IndexPath) -> ASCellNode {
         let cell = FeedCellNode(feed: feeds[indexPath.row])
-        //cell.style.preferredSize.height = 100
         return cell
     }
 
